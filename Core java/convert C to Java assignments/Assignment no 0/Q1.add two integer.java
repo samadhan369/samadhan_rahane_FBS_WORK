6a223117df93952aaa5d7a38cd455326adcfc33f
samadhan maruti rahane <samadhanrahane8@gmail.com>
2025-07-15 16:33:44 +0530

    Create Q1.add two integer.java

---
+++ Core java/convert C to Java assignments/Assignment no 0/Q1.add two integer.java
@@ -0,0 +1,14 @@
+package assignmentno1;
+
+public class que1 {
+
+	public static void main(String[] args) {
+		int a,b,sum;
+		a=5;
+		b=5;
+		sum=a+b;
+		System.out.printf("sum is %d + %d = %d",a,b,sum);
+
+	}
+
+}
